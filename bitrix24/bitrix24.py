1e06f947622519e40e2dba7a590d998e17cee091
Ivan <lichpochta@gmail.com>
2020-08-19 18:03:12 +0300

    fix

--- bitrix24/bitrix24.py
+++ bitrix24/bitrix24.py
@@ -11,7 +11,7 @@ This module implements the Bitrix24 REST API.
 """
 import requests
 from time import sleep
-from urllib.parse import urlparse
+from urlparse import urlparse
 from .exceptions import BitrixError
 
 
